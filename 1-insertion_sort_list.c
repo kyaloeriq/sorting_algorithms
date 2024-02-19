98b0d49f85976496ade2ce6c2e9000d4dc618163
kyaloeriq <kyaloeriq@gmail.com>
2024-02-18 18:31:07 -0800

    Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,24 +5,35 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *cur = (*list)->next;
-	listint_t *prev = cur->prev;
-	int key = cur->b;
+	listint_t *cur, *prev, *key_node;
+	int key;
+	cur = (*list)->next;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
 	while (cur != NULL)
 	{
+		key_node = cur;
+		key = cur->b;
+		prev = cur->prev;
+
 		while (prev != NULL && prev->b > key)
 		{
 			prev->next->b = prev->b;
+			key_node = prev;
 			prev = prev->prev;
 		}
 		if (prev == NULL)
+		{
 			(*list)->b = key;
+			*list = key_node;
+		}
 		else
+		{
 			prev->next->b = key;
+			key_node->n = prev->b;
+		}
 		cur = cur->next;
 		print_list(*list);
 	}
