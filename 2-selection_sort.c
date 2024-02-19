b8acb020bf8f94fb2a709ef87c4c45d37c73835e
kyaloeriq <kyaloeriq@gmail.com>
2024-02-18 17:58:18 -0800

    header file

---
+++ 2-selection_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+/*
+ * selection_sort - sorts an array of integers in using Selection sort
+ * @array: array of integers
+ * @size: size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t a, b;
+	int temp;
+
+
