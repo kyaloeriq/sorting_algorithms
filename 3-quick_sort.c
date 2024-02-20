2530daacb771cea0b285a9f818e0bbb0dde234c8
kyaloeriq <kyaloeriq@gmail.com>
2024-02-19 18:19:53 -0800

    Quick sort algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,19 +1,38 @@
 #include "sort.h"
 /*
- * quick_sort - sorts an array of integers in ascending order using the sort
+ * quick_sort - sorts an array of integers in ascending order using quicksort
  * @array: array of integers
  * @size: size of array
  */
 void quick_sort(int *array, size_t size)
 {
-	int pvt;
-	size_t cur, prev, next;
+	int pvt = array[size - 1];
+	int temp;
+	size_t a, b;
 
-	while (pvt < cur)
+	size_t a = 0;
+	size_t b;
+
+	if (size < 2)
+		return;
+
+	/*partitioning*/
+	for (b = 0; b < size - 1; b++)
 	{
-		cur->next = pvt;
-		pvt = cur->prev;
-		cut->prev = cur->next;
+		if (array[b] < pvt)
+		{	/*swapping array[a] and array[b]*/
+			temp = array[a];
+			array[a] = array[b];
+			array[b] = temp;
+			print_array(array, size);
+			i++;
+		}
 	}
+	/*swapping array[a] and pivot*/
+	array[size - 1] = array[a];
+	array[a] = pvt;
 	print_array(array, size);
+	/*Recursive calls for the two partitions*/
+	quick_sort(array, a);
+	quick_sort(array + a + 1, size - a - 1);
 }	
